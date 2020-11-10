25039341b590b723d0a08bdf389a2b8e7102cbc0
JulioG2021 <71658746+JulioG2021@users.noreply.github.com>
2020-11-10 11:57:30 -0600

    Update fortuneteller.js

--- JS/fortuneteller.js
+++ JS/fortuneteller.js
@@ -1,1 +1,14 @@
+// JavaScript Document
+function tellFortune(numKids, partner, location, jobTitle) {
+    let future = "You will be a " + jobTitle + " in " + location + ", and married to " + partner + " with " + numKids + " kids."
+    return(future)
+    
+}
+   
+tellFortune(2, 'Mark', 'Argentina', "Mechanic");
+tellFortune(3, 'Angelina', 'Brazil', "Doctor");
+tellFortune(1, 'Sebastian', 'France', "Carpenter");
+document.getElementById("fortune1").innerHTML = tellFortune(2, 'Mark', 'Argentina', "Mechanic");
+document.getElementById("fortune2").innerHTML = tellFortune(3, 'Angelina', 'Brazil', "Doctor");
+document.getElementById("fortune3").innerHTML = tellFortune(1, 'Sebastian', 'France', "Carpenter");
 

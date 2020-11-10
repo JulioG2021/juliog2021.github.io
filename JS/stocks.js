55234c3b57c1acdea3fa810b97c3a07d9e07f3d3
JulioG2021 <71658746+JulioG2021@users.noreply.github.com>
2020-11-10 11:47:48 -0600

    Update stocks.js

--- JS/stocks.js
+++ JS/stocks.js
@@ -1,1 +1,13 @@
+// JavaScript Document
+let tesla = 411.66;
+let nike = 128.05;
+let starbucks = 94.32;
+let amazon = 3057.25;
+let apple = 117.01;
 
+	
+  document.getElementById("tsla").innerHTML = "Tesla + " +tesla;
+  document.getElementById("nke").innerHTML = "Nike + " +nike;
+  document.getElementById("sbux").innerHTML = "Starbucks + " +starbucks;
+  document.getElementById("amzn").innerHTML = "Amazon + " +amazon;
+  document.getElementById("aapl").innerHTML = "Apple + " +apple;

a05eb7882d947766c1e77a880af50e642f23a79a
JulioG2021 <71658746+JulioG2021@users.noreply.github.com>
2020-11-21 16:03:26 -0600

    Update stockscopy.js

--- stockscopy.js
+++ stockscopy.js
@@ -1,15 +1,6 @@
+<h2>Fortune Teller</h2>
+			<p id="fortune1"></p>
+			<p id="fortune2"></p>
+			<p id="fortune3" style="padding-bottom: 30px"></p>
 
-
-// JavaScript Document
-let tesla = 411.66;
-let nike = 128.05;
-let starbucks = 94.32;
-let amazon = 3057.25;
-let apple = 117.01;
-
-	
-  document.getElementById("tsla").innerHTML = "Tesla + " +tesla;
-  document.getElementById("nke").innerHTML = "Nike + " +nike;
-  document.getElementById("sbux").innerHTML = "Starbucks + " +starbucks;
-  document.getElementById("amzn").innerHTML = "Amazon + " +amazon;
-  document.getElementById("aapl").innerHTML = "Apple + " +apple;
+<script src="../JS/fortuneTeller.js"></script>
